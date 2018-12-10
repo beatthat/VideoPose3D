ce3bcf6e082725294f05aadac5b1056f24c468b7
larrykirschner <larrykirschner@gmail.com>
2018-12-10 09:30:19 -0800

    docker env for videopose3d

---
+++ run-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+ROOT=`pwd`
+
+NAME=fastai
+
+# mkdir -p docker_host
+HOST_MOUNT=`pwd`
+
+docker run \
+  --name ${NAME} \
+  -it \
+  --rm \
+  --shm-size 16G \
+  --runtime=nvidia \
+  -p 8888:8888 \
+  --mount type=bind,src=${HOST_MOUNT},target=/docker_host \
+  --workdir /docker_host \
+  fastai:1.0
+
+
+# docker exec -it ${NAME} bash
